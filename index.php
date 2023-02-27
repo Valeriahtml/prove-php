a85f110d3126117568472022ff9538b5b4310aeb
Valeria <trnvlra@gmail.com>
2023-02-27 14:57:12 +0100

    aggiunto qualcosa

--- index.php
+++ index.php
@@ -7,9 +7,31 @@
     <title>Document</title>
 </head>
 <body>
+   
     <?php
-    echo "Ciao mondo!"
-    
+        //assegno un valore alla variabile
+           $testo = "ciao mondo"; 
+
+        //stampo il contenuto della variabile
+        echo $testo;
+        //su html verr� mostrato esattamente come una scritta nel body
+
+
+        /*
+        Assegno il valore alla variabile
+        in questo caso la variabile si chiama "testo" e viene scritta sempre con il $
+        il valore sar� Maramiao, quindi va scritto tra virgolette e con l'uguale perch� � appunto un valore
+        */
+        $testo = "Maramiao";
+
+        /*
+        Stampo il valore della variabile
+        qui le virgolette vengono omesse, perch� gi� contenute nel valore della variabile
+        */
+        echo $testo;
+
+        for($i=0; $i<10; $i =$i +1) {echo $testo; }
     ?>
+    
 </body>
 </html>
