627c3ff53880bb2593c254a68f3a723382d14e76
Valeria <trnvlra@gmail.com>
2023-03-01 12:02:19 +0100

    added valori

--- div.php
+++ div.php
@@ -10,14 +10,46 @@
     
 <?php
 
-
 $contenitore = "<div>contenuto<div> ";
 
-
-for ($i=0; $i<10; $i =$i +1) {echo $contenitore . "<br>"; } 
-
+//stampo 10 volte il contenuto della variabile
+for ($i=0; $i<10; $i =$i +1)
+ {echo $contenitore . "<br>"; } 
 
 ?>
+
+<?php
+
+function valore_assoluto($numero){
+    //se il valore è negativo lo moltiplico per -1
+    //altrimenti lo restituisco senza modificarlo
+    if ($numero < 0){
+        $risultato = $numero *-1; 
+    }  else {
+        $risultato = $numero;
+    }
+
+    return $risultato;
+} 
+?>
+<?php 
+//assegno un valore alla variabile
+$valore = -5;
+echo "il valore assoluto di" . $valore;
+
+//stampo il risultato della funzione
+$valore = valore_assoluto ($valore);
+echo "è" . $valore . "<br>";
+
+//assegno un  valore alla variabile
+$nuovo_valore = -7;
+
+//stampo il risultato della funzione
+$valore = valore_assoluto($nuovo_valore);
+echo "è" . $valore;
+
+?>
+
 
 </body>
 </html>
